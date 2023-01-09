126c5fa64e92c8602d15c4b4a5a3c9c8b8f8398e
Gregorio Magini <gmagini@gmail.com>
2023-01-09 15:56:55 +0100

    More basic test setup

--- out/test/suite/extension.test.js
+++ out/test/suite/extension.test.js
@@ -1,13 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const assert = require("assert");
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 const vscode = require("vscode");
 // import * as myExtension from '../../extension';
-suite('Extension Test Suite', () => {
-    vscode.window.showInformationMessage('Start all tests.');
-    test('@ngInject', async () => {
+suite('Test Suite', () => {
+    test('@ngInject Regular Expressions', async () => {
         const doc = await vscode.workspace.openTextDocument({
             language: 'javascript',
         });
@@ -15,7 +13,15 @@ suite('Extension Test Suite', () => {
         await editor.edit(textEdit => {
             textEdit.insert(new vscode.Position(0, 0), 'ciao');
         });
-        assert.strictEqual('ciao', doc.getText());
+        // Could be
+        // an annotated function parameter -> /** @ngInject */ function(foo, bar, baz)
+        // an annotated named function parameter -> /** @ngInject */ function FuncName(foo, bar, baz)
+        // an annotated class constructor parameter --> /** @ngInject */ constructor(foo, bar, baz)
+        // a provider $get method -> /** @ngInject */ $get(foo, bar, baz)
+        // any annotated parameter list --> /** @ngInject */ (foo, bar, baz)
+        // an annotated arrow function single parameter --> /** @ngInject */ foo =>
+        // a jsdoc link in a comment --> /** {@link foo} */
+        // any parameter list followed by string inject --> (foo, bar, baz) { 'ngInject'
     });
 });
 //# sourceMappingURL=extension.test.js.map
